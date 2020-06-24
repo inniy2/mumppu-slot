80f1b9ca5a2c35b1e37c0d766e2b38b51da706b3
inniy2 <inniy2@gmail.com>
2020-06-24 07:28:01 +0300

    Init

--- src/App.js
+++ src/App.js
@@ -2,23 +2,14 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import Spinner from './Spinner';
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" >
+      <Spinner initTime ='2000'></Spinner>
+      <Spinner initTime ='2500'></Spinner>
+      <Spinner initTime ='3000'></Spinner>
     </div>
   );
 }
